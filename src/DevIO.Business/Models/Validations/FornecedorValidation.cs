4f207bf50883bccdf5e03de0234b35a22fcdc1f8
Perk <natspindola@hotmail.com>
2022-07-13 20:01:24 -0300

    Add project files.

---
+++ src/DevIO.Business/Models/Validations/FornecedorValidation.cs
@@ -0,0 +1,31 @@
+﻿using DevIO.Business.Models.Validations.Documentos;
+using FluentValidation;
+
+namespace DevIO.Business.Models.Validations
+{
+    public class FornecedorValidation : AbstractValidator<Fornecedor>
+    {
+        public FornecedorValidation()
+        {
+            RuleFor(expression: f => f.Nome)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caractéres.");
+
+            When(predicate: f => f.TipoFornecedor == TipoFornecedor.PessoaFisica, action: () =>
+            {
+                RuleFor(expression: f => f.Documento.Length).Equal(CpfValidacao.TamanhoCpf)
+                .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}.");
+                RuleFor(expression: f => CpfValidacao.Validar(f.Documento)).Equal(toCompare: true)
+                .WithMessage("O documento fornecido é inválido.");
+            });
+
+            When(predicate: f => f.TipoFornecedor == TipoFornecedor.PessoaJuridica, action: () =>
+            {
+                RuleFor(expression: f => f.Documento.Length).Equal(CnpjValidacao.TamanhoCnpj)
+                .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}.");
+                RuleFor(expression: f => CnpjValidacao.Validar(f.Documento)).Equal(toCompare: true)
+                .WithMessage("O documento fornecido é inválido.");
+            });
+        }
+    }
+}
---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,35 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //se esquecer de mapear algo diretamente no mapping
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                .Where(p => p.ClrType == typeof(string))))
+                property.Relational().ColumnType = "varchar(100)";
+
+            //registrando os mappings na aplicação
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            //desabilitando o cascate delete
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
