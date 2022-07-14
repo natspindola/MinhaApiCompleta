944385e2d18eaa09c5f2d66dbfba8fe4a77094f0
Perk <natspindola@hotmail.com>
2022-07-14 17:28:08 -0300

    Produto View Model

--- src/DevIO.Business/Models/Produto.cs
+++ src/DevIO.Business/Models/Produto.cs
@@ -10,26 +10,11 @@ namespace DevIO.Business.Models
     public class Produto : Entity
     {
         public Guid FornecedorId { get; set; }
-
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 2)]
         public string Nome { get; set; }
-
-        [DisplayName("Descrição")]
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 2)]
         public string Descricao { get; set; }
-
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 2)]
         public string Imagem { get; set; }
-
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Valor { get; set; }
-
-        [ScaffoldColumn(false)]
         public DateTime DataCadastro { get; set; }
-
         public bool Ativo { get; set; }
 
         /* EF Relation */
