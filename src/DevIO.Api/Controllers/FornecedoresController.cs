e112a74af2ed5123ea1f3547a9a50733a09935a1
Perk <natspindola@hotmail.com>
2022-07-15 18:12:50 -0300

    método obter por id

--- src/DevIO.Api/Controllers/FornecedoresController.cs
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -2,6 +2,7 @@
 using DevIO.Api.ViewModels;
 using DevIO.Business.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -21,11 +22,26 @@ namespace DevIO.Api.Controllers
             _mapper = mapper;
         }
 
-
+        [HttpGet]
         public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
         {
             var fornecedor = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
             return fornecedor;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
+        {
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedor == null) return NotFound();
+
+            return fornecedor;
+        }
+
+        public async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
         }
     }
 }
