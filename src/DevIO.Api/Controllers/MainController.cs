acd2209613a3a7865982dde46d2f868ff7298052
Perk <natspindola@hotmail.com>
2022-07-18 19:31:20 -0300

    Notificação de erros

--- src/DevIO.Api/Controllers/MainController.cs
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,31 @@ namespace DevIO.Api.Controllers
     [ApiController]
     public abstract class MainController : ControllerBase
     {
+        private readonly INotificador _notificador;
 
+        protected MainController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+
+        }
+
+        protected void NotificarErroModelInvalida(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var erro in erros)
+            {
+                var erroMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotificarErro(erroMsg);
+            }
+        }
+
+        protected void NotificarErro(string mensagem)
+        {
+            _notificador.Handle(new Business.Notificacoes.Notificacao(mensagem));
+        }
     }
 }
