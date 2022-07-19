e4dde18fb74fbc93014a13854da27a16e5f776f3
Perk <natspindola@hotmail.com>
2022-07-18 21:03:17 -0300

    ProdutosController - injeção de dependência

---
+++ src/DevIO.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DevIO.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    public class ProdutosController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+
+        public ProdutosController(INotificador notificador, 
+                                  IProdutoRepository produtoRepository,
+                                  IProdutoService produtoService,
+                                  IMapper mapper) : base(notificador)
+        {
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+            _mapper = mapper;
+        }
+    }
+}
