9eca3b5066e5c1f4f2ec65d789a7e9eed308de8b
Perk <natspindola@hotmail.com>
2022-07-22 18:23:58 -0300

    Configurando Identity

---
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -0,0 +1,19 @@
+﻿using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.Api.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(optionsAction: options =>
+            options.UseSqlServer(configuration.GetConnectionString(name: "DefaultConnection")));
+
+            return services;
+        }
+    }
+}
--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -22,10 +22,7 @@ namespace DevIO.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MeuDbContext>(optionsAction: options =>
-             {
-                 options.UseSqlServer(Configuration.GetConnectionString(name: "DefaultConnection"));
-             });
+            services.AddIdentityConfiguration(Configuration);
 
             services.AddAutoMapper(typeof(Startup));
 
