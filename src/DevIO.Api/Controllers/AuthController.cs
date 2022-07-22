a2e39b03bffdeae1a772f00aa41ebde774ffce25
Perk <natspindola@hotmail.com>
2022-07-22 18:56:13 -0300

    Controller de autenticação

---
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿
+
+using DevIO.Business.Interfaces;
+
+namespace DevIO.Api.Controllers
+{
+    public class AuthController : MainController
+    {
+        public AuthController(INotificador notificador) : base(notificador)
+        {
+        }
+    }
+}
