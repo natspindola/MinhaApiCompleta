0eacef05d0adbf9e45e8e431cd613f5b5e80c8a2
Perk <natspindola@hotmail.com>
2022-07-25 20:18:39 -0300

    registro de usuário

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -22,7 +22,25 @@ namespace DevIO.Api.Controllers
 
         public async Task<ActionResult> Registrar(RegisterUserViewModel registerUser)
         {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
 
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return CustomResponse(registerUser);
+            }
+            foreach (var error in result.Errors)
+            {
+                NotificarErro(error.Description);
+            }
 
             return CustomResponse(registerUser);
         }
