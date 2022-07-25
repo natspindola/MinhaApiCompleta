ed9378ec9ee31860916343539162e2eaf8bfbfe3
Perk <natspindola@hotmail.com>
2022-07-25 20:05:35 -0300

    LoginUser ViewModel

--- src/DevIO.Api/ViewModels/UserViewModel.cs
+++ src/DevIO.Api/ViewModels/UserViewModel.cs
@@ -6,20 +6,28 @@ using System.Threading.Tasks;
 
 namespace DevIO.Api.ViewModels
 {
-    public class UserViewModel
+    public class RegisterUserViewModel
     {
-        public class RegisterUserViewModel
-        {
-            [Required(ErrorMessage = "O campo {0} é obrigatorio")]
-            [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
-            public string Email { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
 
-            [Required(ErrorMessage = "O campo {0} é obrigatorio")]
-            [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
-            public string Password { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
 
-            [Compare("Password", ErrorMessage = "As senhas não conferem.")]
-            public string ConfirmPassword { get; set; }
-        }
+        [Compare("Password", ErrorMessage = "As senhas não conferem.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginUserViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
     }
 }
