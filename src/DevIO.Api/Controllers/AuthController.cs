02922f53a0b9ecdc379b46bec84a6340070690e3
Perk <natspindola@hotmail.com>
2022-07-25 20:12:47 -0300

    Controller de autenticação

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -1,13 +1,30 @@
-﻿
-
+﻿using DevIO.Api.ViewModels;
 using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace DevIO.Api.Controllers
 {
+    [Route("api/conta")]
     public class AuthController : MainController
     {
-        public AuthController(INotificador notificador) : base(notificador)
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(INotificador notificador,
+                              SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager) : base(notificador)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public async Task<ActionResult> Registrar(RegisterUserViewModel registerUser)
+        {
+
+
+            return CustomResponse(registerUser);
         }
     }
 }
