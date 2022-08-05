825f04d3faaca1629366b51c40aec36b2f3d3a80
Natália Spindola <natspindola@hotmail.com>
2022-08-05 19:35:19 -0300

    Configurações swagger

---
+++ src/DevIO.Api/Configuration/SwaggerConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace DevIO.Api.Configuration
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        readonly IApiVersionDescriptionProvider provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+            }
+        }
+
+        static Info CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new Info()
+            {
+                Title = "API - desenvolvedor.io",
+                Version = description.ApiVersion.ToString(),
+                Description = "Esta API faz parte do curso REST com ASP.NET Core WebAPI.",
+                Contact = new Contact() { Name = "Natalia", Email = "natspindola@hotmail.com" },
+                TermsOfService = "https://opensource.org/licenses/MIT",
+                License = new License() { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
+            };
+
+            if(description.IsDeprecated)
+            {
+                info.Description += "Esta versão está obsoleta!";
+            }
+
+            return info;
+        }
+    }
+}
