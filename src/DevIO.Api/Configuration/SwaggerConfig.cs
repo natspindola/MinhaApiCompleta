80b040ab9cbbe6b8cd8796801a0cf693f823428d
Natália Spindola <natspindola@hotmail.com>
2022-08-05 19:49:29 -0300

    Implementaçãos do Swagger

--- src/DevIO.Api/Configuration/SwaggerConfig.cs
+++ src/DevIO.Api/Configuration/SwaggerConfig.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
 
 namespace DevIO.Api.Configuration
 {
@@ -51,6 +52,33 @@ namespace DevIO.Api.Configuration
             }
 
             return info;
+        }
+    }
+
+    public class SwaggerDefaultValues : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            var apiDescription = context.ApiDescription;
+
+            operation.Deprecated = apiDescription.IsDeprecated();
+
+            if (operation.Parameters == null)
+            {
+                return;
+            }
+
+            foreach (var parameter in operation.Parameters.OfType<NonBodyParameter>())
+            {
+                var description = apiDescription.ParameterDescriptions.First(p => p.Name == parameter.Name);
+
+                if (parameter.Description == null)
+                {
+                    parameter.Description = description.ModelMetadata?.Description;
+                }
+
+                parameter.Required |= description.IsRequired;
+            }
         }
     }
 }
